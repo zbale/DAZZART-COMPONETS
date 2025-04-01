b9f35ec210895c8f02cc3f5fcbbbfcb4c776ef0a
zbale <davidsantiagomoralespachon@gmail.com>
2025-03-31 22:13:12 -0500

    agregando el bootstrap a mis archivos...

---
+++ TRABAJO/JS/login.js
@@ -0,0 +1,5 @@
+document.getElementById('login-btn').addEventListener('click', function () {
+    var myModal = new bootstrap.Modal(document.getElementById('login-modal'));
+    myModal.show();
+  });
+  
