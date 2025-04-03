b1f0ed65a4eb5fb6dc1e5d28734693c8e0480429
zbale <jopsezabaleta5@gmail.com>
2025-04-03 16:16:35 -0500

    Eliminado archivo JS/login.js ya que su funcionalidad fue movida a modal.js

--- TRABAJO/JS/login.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('login-btn').addEventListener('click', function () {
-    var myModal = new bootstrap.Modal(document.getElementById('login-modal'));
-    myModal.show();
-  });
-
-  
-  
