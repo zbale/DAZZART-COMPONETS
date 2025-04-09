f8b6fda26c432a2c588c2916a4ffa0e3c7da1294
Michael <michaelvaskes2017@gmail.com>
2025-04-08 22:20:54 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+class Config:
+    SECRET_KEY = 'CLAVE'
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = '12345'
+    MYSQL_DB = 'DAZZART'
+    SESSION_COOKIE_SECURE = False
+    MYSQL_PORT = 3306  # Asegúrate que coincide con el puerto de XAMPP
+
+
