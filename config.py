0acf7e9e3b7d30ce179c0f0f6bb580ebdb67916d
Jose David Daza <99748316+JoseSama10@users.noreply.github.com>
2025-04-10 08:58:04 -0500

    subir config.py y run.py
    subiendo archivos importantes

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    SECRET_KEY = 'CLAVE'
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'SENA'
+    MYSQL_DB = 'DAZZART'
+    SESSION_COOKIE_SECURE: False
