5c01901cc65fee535b3da9aeb231232bab91edd8
Michael <michaelvaskes2017@gmail.com>
2025-04-10 21:12:33 -0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    SECRET_KEY = 'CLAVE'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'SENA'
-    MYSQL_DB = 'DAZZART'
-    SESSION_COOKIE_SECURE: False
