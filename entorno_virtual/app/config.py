8ef90ed915c1ef85df1dbc3023a51d94f29f49b3
jose zabaleta <jopsezabaleta5@gmail.com>
2025-04-21 07:35:33 -0500

    Delete mi entorno_virtual directory

--- entorno_virtual/app/config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    SECRET_KEY = 'Elcoste022024'  
-    MYSQL_HOST = '127.0.0.1'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'SENA'  
-    MYSQL_DB = 'DAZZART'   
-    SESSION_COOKIE_SECURE = False 
--- entorno_virtual/app/controllers/user_controller.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-user_bp = Blueprint('user', __name__, url_prefix='/user')
-
-@user_bp.route('/login')
-def login():
-    return render_template('Index/login.html')
-
