122d7046b17bb71f1e9729f06fb3d7eb7991bdae
jose zabaleta <jopsezabaleta5@gmail.com>
2025-04-21 07:49:23 -0500

    carpetas _satic

--- app/static/JS/modal.js
+++ app/static/JS/modal.js
@@ -4,49 +4,4 @@ document.addEventListener("DOMContentLoaded", function () {
         var myModal = new bootstrap.Modal(document.getElementById("login-modal"));
         myModal.show();
     });
-
-    // el manejo del formulario de login
-    document.querySelector("form").addEventListener("submit", function (event) {
-        event.preventDefault(); 
-
-        const email = document.getElementById("email").value.trim();
-        const password = document.getElementById("password").value.trim();
-
-
-
-        // aqui se verifica si los campos están vacíos
-        if (email === "" || password === "") {
-            Swal.fire({
-                icon: "warning",
-                title: "Campos vacíos",
-                text: "Por favor, completa todos los campos antes de continuar.",
-                confirmButtonText: "Aceptar",
-            });
-            return;
-        }
-
-        const usuarios = {
-            "admin@valecita.com": "2024",
-            "josedaviddazacamacho@gmail.com": "2024",
-        };
-
-        // se valida si el usuario y la contraseña son correctos
-        if (usuarios[email] && usuarios[email] === password) {
-            Swal.fire({
-                icon: "success",
-                title: "Inicio Exitoso!",
-                text: email === "admin@valecita.com" ? "Bienvenido Administrador." : "Bienvenido!",
-                confirmButtonText: "Ingresar",
-            }).then(() => {
-                window.location.href = "../admin/categorias.html";
-            });
-        } else {
-            Swal.fire({
-                icon: "error",
-                title: "Credenciales Incorrectas",
-                text: "El correo o la contraseña no son válidos.",
-                confirmButtonText: "Intentar de nuevo",
-            });
-        }
-    });
 });
